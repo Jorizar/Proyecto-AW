9831e391c1b789e9c1c5a6aa2a19444dc3f2ecc1
fedelm8 <fedelmerino8@gmail.com>
2024-03-25 20:10:27 +0100

    plantilla

--- includes/vistas/plantillas/plantilla.php
+++ includes/vistas/plantillas/plantilla.php
@@ -3,25 +3,23 @@ $params['app']->doInclude('/vistas/helpers/plantilla.php');
 $mensajes = mensajesPeticionAnterior();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-	<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <title><?= $params['tituloPagina'] ?></title>
-	<link rel="stylesheet" type="text/css" href="<?= $params['app']->resuelve('/css/estilo.css') ?>" /></head>
+    <link rel="stylesheet" type="text/css" href="<?= $params['app']->resuelve('/css/estilo.css') ?>">
+</head>
 <body>
-<?= $mensajes ?>
-<div id="contenedor">
-<?php
-$params['app']->doInclude('/vistas/comun/cabecera.php', $params);
-?>
-	<main>
-		<article>
-			<?= $params['contenidoPrincipal'] ?>
-		</article>
-	</main>
-<?php
-$params['app']->doInclude('/vistas/comun/pie.php', $params);
-?>
-</div>
+    <?= $mensajes ?>
+    <div id="contenedor">
+        <?php $params['app']->doInclude('/vistas/comun/cabecera.php', $params); ?>
+        <main>
+            <article>
+                <?= $params['contenidoPrincipal'] ?>
+            </article>
+        </main>
+        <?php $params['app']->doInclude('/vistas/comun/pie.php', $params); ?>
+    </div>
 </body>
 </html>
+
