1b912d3a5e199aabb7072da083b42a213089a189
fedelm8 <fedelmerino8@gmail.com>
2024-04-08 17:42:56 +0200

    perfil arreglado

--- perfil.php
+++ perfil.php
@@ -16,30 +16,31 @@ if ($app->usuarioLogueado()) {
 
     $contenidoPrincipal=<<<EOS
     <h2>Mi Perfil</h2>
-<div style="display: flex; align-items: center;">
-    <div style="margin-right: 20px;">
-        <img src="${fotoPerfil}" alt='Foto de perfil' width='100' height='100'>;
+    <div style="display: flex; align-items: center;">
+        <div style="margin-right: 20px;">
+            <img src="${fotoPerfil}" alt='Foto de perfil' width='100' height='100'>;
+        </div>
+        <div>
+            <p>@usuario: ${nombreUsuario}</p>;                        
+            <p>Plan: ${plan}</p>;
+        </div>
     </div>
-    <div>
-        <p>@usuario: ${nombreUsuario}</p>;                        
-        <p>Plan: ${plan}</p>;
+    <div style="margin-top: 20px;">
+        <div style="text-align: right;">
+            <a href="${urlDatos}">Cambiar Datos</a><br>
+            <a href="${urlPlan}">Cambiar Plan</a><br>
+            <a href="${urlComentarios}">Mis Comentarios</a><br>
+            <a href="${urlListas}">Mis Listas</a><br>
+        </div>
     </div>
-</div>
-<div style="margin-top: 20px;">
-    <div style="text-align: right;">
-        <a href="${urlDatos}">Cambiar Datos</a><br>
-        <a href="${urlPlan}">Cambiar Plan</a><br>
-        <a href="${urlComentarios}">Mis Comentarios</a><br>
-        <a href="${urlListas}">Mis Listas</a><br>
-    </div>
-</div>
-  EOS;
+EOS;
 } else {
   $contenidoPrincipal=<<<EOS
     <h1>Usuario no registrado!</h1>
     <p>Debes iniciar sesión para ver el contenido.</p>
-  EOS;
+EOS;
 }
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
 $app->generaVista('/plantillas/plantilla.php', $params);
+?>
