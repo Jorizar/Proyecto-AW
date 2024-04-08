29bb2d2c46fff8f2e862e1b2120f3455fb6ee3b0
ElMariones <45536403+ElMariones@users.noreply.github.com>
2024-04-08 12:08:36 +0200

    Add files via upload

---
+++ cabecera.php
@@ -0,0 +1,48 @@
+<?php
+
+use es\ucm\fdi\aw\Aplicacion;
+use es\ucm\fdi\aw\usuarios\FormularioLogout;
+$app = Aplicacion::getInstance();
+
+function mostrarSaludo()
+{
+    $html = '';
+    $app = Aplicacion::getInstance();
+    if ($app->usuarioLogueado()) {
+        $nombreUsuario = $app->nombreUsuario();
+
+        $formLogout = new FormularioLogout();
+        $htmlLogout = $formLogout->gestiona();
+        $html = "Bienvenido, ${nombreUsuario}. $htmlLogout";
+    } else {
+        $loginUrl = $app->resuelve('/login.php');
+        $registroUrl = $app->resuelve('/registro.php');
+        $html = <<<EOS
+        Usuario desconocido. <a href="{$loginUrl}">Login</a> <a href="{$registroUrl}">Registro</a>
+      EOS;
+    }
+
+    return $html;
+}
+
+?>
+
+<body>
+<header>
+    <div class ="logo">
+        <img src="./img/logo_proyecto.jpg" alt="Logo">
+    </div>
+    <div class="saludo-menu-container"> <!-- Contenedor para el saludo y el menú -->
+        <nav class="menu">
+            <a href="<?= $app->resuelve('/index.php')?>">Inicio</a>
+            <a href="<?= $app->resuelve('/admin.php')?>">Administrar</a>
+            <a href="<?= $app->resuelve('/noticias.php')?>">Noticias</a>
+            <a href="<?= $app->resuelve('/peliculas_fav.php')?>"> Películas <img src="/img/fav.png" alt="Películas" width="50" height="50"></a>
+            <a href="<?= $app->resuelve('/perfil.php')?>">Perfil</a>
+        </nav>
+        <div class="saludo">
+            <?= mostrarSaludo(); ?>
+        </div>
+    </div>
+</header>
+</body>
