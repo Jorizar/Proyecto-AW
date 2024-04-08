4d1b649774806016d40285a232e28f239c10542a
fedelm8 <fedelmerino8@gmail.com>
2024-04-08 19:52:32 +0200

    bd

--- mysql/estructura.sql
+++ mysql/estructura.sql
@@ -1,27 +1,85 @@
 /*
   Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
 */
-DROP TABLE IF EXISTS `RolesUsuario`;
-DROP TABLE IF EXISTS `Roles`;
-DROP TABLE IF EXISTS `Usuarios`;
+DROP TABLE IF EXISTS `comentarios`;
+DROP TABLE IF EXISTS `favoritos`;
+DROP TABLE IF EXISTS `listas`;
+DROP TABLE IF EXISTS `noticias`;
+DROP TABLE IF EXISTS `usuarios`;
+DROP TABLE IF EXISTS `peliculas`;
 
-CREATE TABLE IF NOT EXISTS `Roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `RolesUsuario` (
-  `usuario` int(11) NOT NULL,
-  `rol` int(11) NOT NULL,
-  PRIMARY KEY (`usuario`,`rol`),
-  KEY `rol` (`rol`)
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE IF NOT EXISTS `comentarios` (
+  `comentario_id` int(3) NOT NULL,
+  `user_id` int(2) UNSIGNED NOT NULL,
+  `pelicula_id` int(2) UNSIGNED NOT NULL,
+  `texto` text NOT NULL,
+  `valoracion` int(2) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS `Usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombreUsuario` varchar(30) COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
-  `password` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
-  `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Estructura de tabla para la tabla `favoritos`
+--
+
+CREATE TABLE IF NOT EXISTS `favoritos` (
+  `user_id` int(2) UNSIGNED NOT NULL,
+  `pelicula_id` int(2) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Estructura de tabla para la tabla `listas`
+--
+
+CREATE TABLE IF NOT EXISTS `listas` (
+  `user_id` int(2) UNSIGNED NOT NULL,
+  `pelicula_id` int(2) UNSIGNED NOT NULL,
+  `lista_id` int(2) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Estructura de tabla para la tabla `noticias`
+--
+
+CREATE TABLE IF NOT EXISTS `noticias` (
+  `titulo` varchar(100) NOT NULL,
+  `post_id` int(2) UNSIGNED NOT NULL,
+  `portada` varchar(50) DEFAULT NULL,
+  `texto` text NOT NULL,
+  `autor` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `titulo` varchar(60) NOT NULL,
+  `director` varchar(35) NOT NULL,
+  `id` int(2) UNSIGNED NOT NULL,
+  `annio` year(4) NOT NULL,
+  `genero` varchar(20) NOT NULL,
+  `sinopsis` text NOT NULL,
+  `portada` varchar(100) NOT NULL,
+  `reparto` longtext NOT NULL,
+  `Val_IMDb` decimal(2,1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `user_id` int(2) UNSIGNED NOT NULL,
+  `rol` varchar(20) NOT NULL,
+  `email` varchar(35) DEFAULT NULL,
+  `foto` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
