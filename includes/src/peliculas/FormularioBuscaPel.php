033cf22c5e4f4a0c6df2b7f97a879ebc166e16f6
SerGii08 <enero2003cs@gmail.com>
2024-04-09 18:34:02 +0200

    Arreglos en el buscador de Películas , añadido el  buscador en el index y arreglos en la clase Pelicula

--- includes/src/peliculas/FormularioBuscaPel.php
+++ includes/src/peliculas/FormularioBuscaPel.php
@@ -19,70 +19,59 @@ class FormularioBuscaPel extends Formulario
     {
          //Obtenemos los géneros de las películas
          $generos = Pelicula::getGeneros();
-         if($generos === FALSE){
-
-         }
+        
          // Se generan los mensajes de error si existen.
          $htmlErroresGlobales = self::generaListaErroresGlobales($this->errores);
-         $erroresCampos = self::generaErroresCampos(['tituloPelicula', 'genero', 'annio', 'director'], $this->errores, 'span', array('class' => 'error'));
+         $erroresCampos = self::generaErroresCampos(['tituloPelicula', 'generoPelicula', 'annioPelicula', 'directorPelicula'], $this->errores, 'span', array('class' => 'error'));
  
          // Se genera el HTML asociado a los campos del formulario y los mensajes de error.
-         $html = <<<EOF
+         $html = <<<EOS
          $htmlErroresGlobales
          <fieldset>
              <legend>Introduce los datos de la película en los campos que te interesen</legend>
              <div>
                  <label for="tituloPelicula">Título:</label>
                  <input id="tituloPelicula" type="text" name="tituloPelicula"/>
-                 {$erroresCampos['tituloPelicula']}
              </div>
              <div>
-                 <label for="genero">Género:</label>
+                <label for="directorPelicula">Director:</label>
+                <input id="directorPelicula" type="text" name="directorPelicula"/>
+            </div>
+            <div>
+                 <label for="generoPelicula">Género:</label>
                  <select id="generoPelicula" name="generoPelicula">
                  <option value="-1">Seleccionar</option>
-                 EOF;
-                 foreach ($generos as $id => $genero){
-                    $html .= "<option value='$id'>$genero</option>";
-                }
+        EOS;
+                 if($generos != FALSE){
+                    foreach ($generos as $id => $genero){
+                        $html .= "<option value='$id'>$genero</option>";
+                    }
+                    $html .= "</select></div>";
+                 }
             $html .= <<<EOF
-                 {$erroresCampos['generoPelicula']}
-             </div>
-             <div>
-                 <label for="directorPelicula">Director:</label>
-                 <input id="directorPelicula" type="text" name="directorPelicula"/>
-                 {$erroresCampos['directorPelicula']}
-             </div>
              <div>
                  <label for="annioPelicula">Año de estreno:</label>
                  <input type="number" id="annioPelicula" name="annioPelicula"/>
-                 {$erroresCampos['annioPelicula']}
              </div>
              <div>
                  <button type="submit" name="buscar">Buscar</button>
              </div>
          </fieldset>
-         EOF;
+        EOF;
          return $html;
     }
     
-    //TO DO
     protected function procesaFormulario(&$datos)
     {
-        $this->errores = [];
-        $tituloPelicula = filter_var($n, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        if ( ! $nombreUsuario || empty($nombreUsuario) ) {
-            $this->errores['nombreUsuario'] = 'El nombre de usuario no puede estar vacío';
-        }
-        
         // Recoger los datos del formulario
-        $titulo = isset($datos['tituloPelicula']) ? $datos['tituloPelicula'] : '';
-        $director = isset($datos['directorPelicula']) ? $datos['directorPelicula'] : '';
-        $genero = ($datos['generoPelicula'] != -1) ? $datos['generoPelicula'] : '';
-        $anno = isset($datos['annioPelicula']) ? $datos['annioPelicula'] : '';
+        $tituloPelicula = isset($datos['tituloPelicula']) ? $datos['tituloPelicula'] : '';
+        $directorPelicula = isset($datos['directorPelicula']) ? $datos['directorPelicula'] : '';
+        $generoPelicula = ($datos['generoPelicula'] != -1) ? $datos['generoPelicula'] : '';
+        $annioPelicula = isset($datos['annioPelicula']) ? $datos['annioPelicula'] : '';
 
         if (count($this->errores) === 0) {
             //$peliculas contiene un array de películas si la búsqueda ha encontrado alguna coincidencia, false en caso contrario
-            $peliculas = Pelicula::buscaPelicula($tituloPelicula, $directorPelicula, $generoPelicula, $annio);
+            $peliculas = Pelicula::buscaPelicula($tituloPelicula, $directorPelicula, $generoPelicula, $annioPelicula);
         
             if ($peliculas === false) {
                 $this->errores[] = "No existen películas con esos criterios de búsqueda";
