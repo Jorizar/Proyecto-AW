2d34f60cd6bdb54f2c9655e1f16fd782c96d3b66
ElMariones <45536403+ElMariones@users.noreply.github.com>
2024-04-09 13:47:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $contenidoPrincipal=<<<EOS
   <h2>Página principal</h2>
   <p> Aquí está el contenido público, visible para todos los usuarios. </p>
   <a href="${login}">Login</a>
-  <form action="pelicula.php" method="get">
+  <form action="vista_pelicula.php" method="get">
     <input type="hidden" name="id" value="1">
     <button type="submit">Go to Movie</button>
   </form>
