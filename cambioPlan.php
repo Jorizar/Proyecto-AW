e0d9bfdfc7d46e8af8bebe8a08c40cf75a9b583a
fedelm8 <fedelmerino8@gmail.com>
2024-04-09 15:45:56 +0200

    cambiarPlan OK

---
+++ cambioPlan.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__.'/includes/config.php';
+
+$formCambioPlan = new \es\ucm\fdi\aw\usuarios\FormularioCambioPlan();
+$formCambioPlan = $formCambioPlan->gestiona();
+
+
+$tituloPagina = 'Cambiar Plan';
+$contenidoPrincipal=<<<EOF
+  	<h1>Acceso al sistema</h1>
+    $formCambioPlan
+EOF;
+
+$params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
+$app->generaVista('/plantillas/plantilla.php', $params);
