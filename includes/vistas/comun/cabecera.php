bdbf279686d0031320bb20bb035e3a5da893f31b
fedelm8 <fedelmerino8@gmail.com>
2024-04-10 09:23:02 +0200

    cabecera con foto y nombre de perfil

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -12,8 +12,14 @@ function mostrarPerfil()
     if ($app->usuarioLogueado()) {
         $fotoPerfil = $app->fotoPerfil();
         $perfilUrl = $app->resuelve('/perfil.php');
+        $nombrePerfil = $app->nombreUsuario();
 
-        $html = "<a href='$perfilUrl'><img src='$fotoPerfil' alt='Foto de perfil' class='imagen-perfil'></a>";
+        $html .= "<div class='perfil-info'>";
+        $html .= "<a href='$perfilUrl'><img src='$fotoPerfil' alt='Foto de perfil' class='imagen-perfil'></a>";
+        $html .= "<a href='$perfilUrl' class='nombre-perfil'>$nombrePerfil</a>"; 
+        $html .= "</div>";
+
+        //$html = "<a href='$perfilUrl'><img src='$fotoPerfil' alt='Foto de perfil' class='imagen-perfil'></a>";
     } else {
         // Si el usuario no está logueado, mostrar opciones de inicio de sesión y registro
         $loginUrl = $app->resuelve('/login.php');
