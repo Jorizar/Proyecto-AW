d4db1f8925a08cf43a3ae61cca757e761b4aef02
ElMariones <45536403+ElMariones@users.noreply.github.com>
2024-04-10 17:05:08 +0200

    Add files via upload

---
+++ includes/src/peliculas/eliminar_pelicula.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__. '/../../config.php';
+require_once __DIR__.'/Pelicula.php';
+
+use es\ucm\fdi\aw\peliculas\Pelicula;
+
+if (!$app->tieneRol('admin')) {
+    echo "Acceso restringido solo a administradores.";
+    exit();
+}
+
+$pelicula_id = filter_input(INPUT_POST, 'pelicula_id', FILTER_SANITIZE_NUMBER_INT);
+
+if (empty($pelicula_id)) {
+    echo "Error: La película no puede ser identificada.";
+    exit();
+}
+
+$resultado = Pelicula::borraPorId($pelicula_id);
+
+if ($resultado) {
+    $relativePath = '/AW/Proyecto-AW/admin_peliculas.php';
+    header('Location: ' . $relativePath);
+    exit();
+} else {
+    echo "Error: No se pudo eliminar la película.";
+    exit();
+}
+?>
