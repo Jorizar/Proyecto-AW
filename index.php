c07d0f229d331eac7c50aa9ccae1990f690b2d45
fedelm8 <fedelmerino8@gmail.com>
2024-04-12 11:23:36 +0200

    mas pelis al inicio y toque de css

--- index.php
+++ index.php
@@ -45,12 +45,13 @@ if ($resultMejorValoradas && $resultMejorValoradas->num_rows > 0) {
         $id = $row['id'];
         $titulo = $row['titulo'];
         $imagen = $row['portada'];
+        $valoracion = $row['Val_IMDb'];
         // Enlace por cada película que redirige a la vista de la película
         $contenidoPrincipal .= <<<HTML
             <div class="pelicula">
                 <a href="vista_pelicula.php?id=$id">
                     <img src="$imagen" alt="$titulo">
-                    <span>$titulo</span>
+                    <span>$titulo ($valoracion)</span>
                 </a>
             </div>
 HTML;
@@ -281,6 +282,137 @@ HTML;
     }
 }
 
+$queryGenero7 = "SELECT id, titulo, portada
+                FROM peliculas
+                WHERE genero = 7
+                ORDER BY Val_IMDb DESC
+                LIMIT 4";
+
+$resultGenero7 = $conexion->query($queryGenero7);
+
+if ($resultGenero7 && $resultGenero7->num_rows > 0) {
+    // Agregar el encabezado "Películas del género 1"
+    $contenidoPrincipal .= '<div class="destacadas">';
+    $contenidoPrincipal .= '<h1>Películas de Terror</h1>';
+    $contenidoPrincipal .= '</div>';
+
+    // Continuar con el contenido principal
+    $contenidoPrincipal .= '<div class="peliculas-container">';
+    while ($row = $resultGenero7->fetch_assoc()) {
+        $id = $row['id'];
+        $titulo = $row['titulo'];
+        $imagen = $row['portada'];
+        // Enlace por cada película que redirige a la vista de la película
+        $contenidoPrincipal .= <<<HTML
+            <div class="pelicula">
+                <a href="vista_pelicula.php?id=$id">
+                    <img src="$imagen" alt="$titulo">
+                    <span>$titulo</span>
+                </a>
+            </div>
+
+HTML;
+    }
+}
+
+$queryGenero8 = "SELECT id, titulo, portada
+                FROM peliculas
+                WHERE genero = 8
+                ORDER BY Val_IMDb DESC
+                LIMIT 4";
+
+$resultGenero8 = $conexion->query($queryGenero8);
+
+if ($resultGenero8 && $resultGenero8->num_rows > 0) {
+    // Agregar el encabezado "Películas del género 1"
+    $contenidoPrincipal .= '<div class="destacadas">';
+    $contenidoPrincipal .= '<h1>Películas Basadas en Hechos Reales</h1>';
+    $contenidoPrincipal .= '</div>';
+
+    // Continuar con el contenido principal
+    $contenidoPrincipal .= '<div class="peliculas-container">';
+    while ($row = $resultGenero8->fetch_assoc()) {
+        $id = $row['id'];
+        $titulo = $row['titulo'];
+        $imagen = $row['portada'];
+        // Enlace por cada película que redirige a la vista de la película
+        $contenidoPrincipal .= <<<HTML
+            <div class="pelicula">
+                <a href="vista_pelicula.php?id=$id">
+                    <img src="$imagen" alt="$titulo">
+                    <span>$titulo</span>
+                </a>
+            </div>
+
+HTML;
+    }
+}
+
+$queryGenero9 = "SELECT id, titulo, portada
+                FROM peliculas
+                WHERE genero = 9
+                ORDER BY Val_IMDb DESC
+                LIMIT 4";
+
+$resultGenero9 = $conexion->query($queryGenero9);
+
+if ($resultGenero9 && $resultGenero9->num_rows > 0) {
+    // Agregar el encabezado "Películas del género 1"
+    $contenidoPrincipal .= '<div class="destacadas">';
+    $contenidoPrincipal .= '<h1>Películas sobre Crímenes</h1>';
+    $contenidoPrincipal .= '</div>';
+
+    // Continuar con el contenido principal
+    $contenidoPrincipal .= '<div class="peliculas-container">';
+    while ($row = $resultGenero9->fetch_assoc()) {
+        $id = $row['id'];
+        $titulo = $row['titulo'];
+        $imagen = $row['portada'];
+        // Enlace por cada película que redirige a la vista de la película
+        $contenidoPrincipal .= <<<HTML
+            <div class="pelicula">
+                <a href="vista_pelicula.php?id=$id">
+                    <img src="$imagen" alt="$titulo">
+                    <span>$titulo</span>
+                </a>
+            </div>
+
+HTML;
+    }
+}
+
+$queryActual = "SELECT id, titulo, portada
+                FROM peliculas
+                WHERE annio = 2024 
+                ORDER BY annio ASC
+                LIMIT 4";
+
+$resultActual = $conexion->query($queryActual);
+
+if ($resultActual && $resultActual->num_rows > 0) {
+    // Agregar el encabezado "Películas de la década de los 80"
+    $contenidoPrincipal .= '<div class="destacadas">';
+    $contenidoPrincipal .= '<h1>Películas de este año</h1>';
+    $contenidoPrincipal .= '</div>';
+
+    // Continuar con el contenido principal
+    $contenidoPrincipal .= '<div class="peliculas-container">';
+    while ($row = $resultActual->fetch_assoc()) {
+        $id = $row['id'];
+        $titulo = $row['titulo'];
+        $imagen = $row['portada'];
+        // Enlace por cada película que redirige a la vista de la película
+        $contenidoPrincipal .= <<<HTML
+            <div class="pelicula">
+                <a href="vista_pelicula.php?id=$id">
+                    <img src="$imagen" alt="$titulo">
+                    <span>$titulo</span>
+                </a>
+            </div>
+HTML;
+}
+}
+
     $queryDecada80s = "SELECT id, titulo, portada
                 FROM peliculas
                 WHERE annio >= 1980 AND annio < 1990
@@ -311,6 +443,71 @@ if ($resultDecada80s && $resultDecada80s->num_rows > 0) {
             </div>
 HTML;
 }
+}
+
+$queryDecada90s = "SELECT id, titulo, portada
+                FROM peliculas
+                WHERE annio >= 1990 AND annio < 2000
+                ORDER BY annio ASC
+                LIMIT 4";
+
+$resultDecada90s = $conexion->query($queryDecada90s);
+
+if ($resultDecada90s && $resultDecada90s->num_rows > 0) {
+    // Agregar el encabezado "Películas de la década de los 80"
+    $contenidoPrincipal .= '<div class="destacadas">';
+    $contenidoPrincipal .= '<h1>Películas de la década de los 90</h1>';
+    $contenidoPrincipal .= '</div>';
+
+    // Continuar con el contenido principal
+    $contenidoPrincipal .= '<div class="peliculas-container">';
+    while ($row = $resultDecada90s->fetch_assoc()) {
+        $id = $row['id'];
+        $titulo = $row['titulo'];
+        $imagen = $row['portada'];
+        // Enlace por cada película que redirige a la vista de la película
+        $contenidoPrincipal .= <<<HTML
+            <div class="pelicula">
+                <a href="vista_pelicula.php?id=$id">
+                    <img src="$imagen" alt="$titulo">
+                    <span>$titulo</span>
+                </a>
+            </div>
+HTML;
+}
+
+$queryDecada2000s = "SELECT id, titulo, portada
+                FROM peliculas
+                WHERE annio >= 2000 AND annio < 2010
+                ORDER BY annio ASC
+                LIMIT 4";
+
+$resultDecada82000s = $conexion->query($queryDecada2000s);
+
+if ($resultDecada82000s && $resultDecada82000s->num_rows > 0) {
+    // Agregar el encabezado "Películas de la década de los 80"
+    $contenidoPrincipal .= '<div class="destacadas">';
+    $contenidoPrincipal .= '<h1>Películas de la década de los 2000</h1>';
+    $contenidoPrincipal .= '</div>';
+
+    // Continuar con el contenido principal
+    $contenidoPrincipal .= '<div class="peliculas-container">';
+    while ($row = $resultDecada82000s->fetch_assoc()) {
+        $id = $row['id'];
+        $titulo = $row['titulo'];
+        $imagen = $row['portada'];
+        // Enlace por cada película que redirige a la vista de la película
+        $contenidoPrincipal .= <<<HTML
+            <div class="pelicula">
+                <a href="vista_pelicula.php?id=$id">
+                    <img src="$imagen" alt="$titulo">
+                    <span>$titulo</span>
+                </a>
+            </div>
+HTML;
+}
+}
+
 $contenidoPrincipal .= '</div>';
     } else {
         $contenidoPrincipal .= "<p>No hay peliculas.</p>";
