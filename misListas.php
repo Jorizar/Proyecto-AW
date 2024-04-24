3e38beb1135e8a58c5faceff9272173e106c4829
SerGii08 <enero2003cs@gmail.com>
2024-04-24 11:06:02 +0200

    Consultas necesarias para acceder a las listas de usuario, así como a las películas de la lista

--- misListas.php
+++ misListas.php
@@ -1,14 +1,31 @@
 <?php
 require_once __DIR__.'/includes/config.php';
 
+use \es\ucm\fdi\aw\peliculas\Pelicula;
+
+
 $tituloPagina = 'Contenido';
 $contenidoPrincipal='';
 
 if ($app->usuarioLogueado()) {
-  $contenidoPrincipal=<<<EOS
+  //Cargamos las listas del usuario
+  $listas = Pelicula::getListasUser($_SESSION['idUsuario']);
+  if($listas == FALSE){
+    $contenidoPrincipal= <<<EOS
     <h2>Mis listas de películas</h2>
-    <p>Aquí podrás crear listas de películas, así como eliminar y agregar películas a estas</p>
+    <p>No has creado ninguna lista todavía</p>
+    EOS;
+  }
+  else{
+    //Mostramos al usuario las listas que ha creado
+    $contenidoPrincipal= '<h2>Mis listas de películas</h2>';
+    foreach($listas as $lista){
+      
+    }
+    
   EOS;
+  }
+
 } else {
   $contenidoPrincipal=<<<EOS
     <h2>Usuario no registrado!</h2>
