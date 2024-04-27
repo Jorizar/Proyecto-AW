d90569998b4a8adddbc305a57d9d5ded4c35365a
SerGii08 <enero2003cs@gmail.com>
2024-04-27 19:59:33 +0200

    Movida la función al js principal

--- js/main.js
+++ js/main.js
@@ -80,3 +80,10 @@ function buscarPeliculas() {
 		}
 	});
 }
+
+//Botón para ver la lista
+function verModificarLista(button, id_lista) {
+    // Redirigir a otra página pasando el nombre de la lista como parámetro
+    window.location.href = "ver_lista.php?id=" + id_lista;
+}
+
