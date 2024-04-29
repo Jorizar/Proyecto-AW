03deb4cb3d33bff48ecf83206f70317982cbac58
pausevi <72093000+pausevi@users.noreply.github.com>
2024-04-29 17:15:34 +0200

    Toda la funcionalidad Admin User

--- cambioDatos.php
+++ cambioDatos.php
@@ -3,20 +3,44 @@
 require_once __DIR__.'/includes/config.php';
 
 use es\ucm\fdi\aw\usuarios\FormularioCambioDatos;
-
-$formCambioDatos = new FormularioCambioDatos();
-$formCambioDatos = $formCambioDatos->gestiona();
+use es\ucm\fdi\aw\usuarios\FormularioCambioAdmin;
 
 
-$tituloPagina = 'Cambiar Datos';
-$contenidoPrincipal=<<<EOF
-  <div class="titulo_cambiarDatos">
-      <h1>Cambiar Datos</h1>
-  </div>
-  <div class="contenedor_cambiarDatos">
-      $formCambioDatos
-  </div>
-EOF;
+if (!$app->tieneRol('admin')) {
+
+  $formCambioDatos = new FormularioCambioDatos();
+  $formCambioDatos = $formCambioDatos->gestiona();
+
+  $tituloPagina = 'Cambiar Datos';
+  $contenidoPrincipal=<<<EOF
+    <div class="titulo_cambiarDatos">
+        <h1>Cambiar Datos</h1>
+    </div>
+    <div class="contenedor_cambiarDatos">
+        $formCambioDatos
+    </div>
+  EOF;
+}
+else{
+
+  // Obtenemos el id de usuario
+  $usuario_id = filter_input(INPUT_POST, 'usuario_id', FILTER_SANITIZE_NUMBER_INT);
+ 
+  $formCambioDatosAdmin = new FormularioCambioAdmin($usuario_id);
+  $formCambioDatosAdmin = $formCambioDatosAdmin->gestiona();
+
+  //$_SESSION['idUsuario'] = $usuario_id;
+  $tituloPagina = 'Cambio de Datos Administrador';
+    $contenidoPrincipal=<<<EOF
+    <div class="titulo_cambiarDatos">
+        <h1>Cambio de Datos Administrador</h1>
+    </div>
+    <div class="contenedor_cambiarDatos">
+        $formCambioDatosAdmin
+    </div>
+  EOF;
+}
+
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
 $app->generaVista('/plantillas/plantilla.php', $params);
---
+++ includes/src/usuarios/FormularioCambioAdmin.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace es\ucm\fdi\aw\usuarios;
+
+use es\ucm\fdi\aw\Aplicacion;
+use es\ucm\fdi\aw\Formulario;
+use es\ucm\fdi\aw\usuarios\Usuario;
+
+class FormularioCambioAdmin extends Formulario
+{
+    protected $id = " ";
+
+    public function __construct($idu)
+    {
+        parent::__construct('formCambioDatos', ['urlRedireccion' => Aplicacion::getInstance()->resuelve('/admin_usuarios.php'), 'enctype' => 'multipart/form-data']);
+        $this->id = $idu;
+    }
+
+    protected function generaCamposFormulario(&$datos)
+    {
+        $htmlUsuarioId= '';
+
+        if (!empty($this->id)) {
+            $htmlUsuarioId = "<input type='hidden' name='usuario_id' value='{$this->id}'>";
+        }
+
+        $erroresCampos = self::generaErroresCampos(['nuevo_nombre', 'nuevo_email'], $this->errores, 'span', array('class' => 'error'));
+        // Se generan los campos del formulario para cambiar los datos del usuario.
+        $html = <<<EOF
+        <div class="titulo_cambiarDatos">
+        </div>
+        <div class="contenedor_cambiarDatos">
+            <div class="cambiar-datos-formulario">
+                    {$htmlUsuarioId}
+                <div class="nombre_cambioPlan">
+                    <label for="nuevo_nombre">Nuevo Nombre:</label>
+                    <input type="text" id="nuevo_nombre" name="nuevo_nombre">
+                    {$erroresCampos['nuevo_nombre']}
+                </div>
+                <div class="email_cambioPlan">
+                    <label for="nuevo_email">Nuevo Correo Electrónico:</label>
+                    <input type="email" id="nuevo_email" name="nuevo_email">
+                    {$erroresCampos['nuevo_email']}
+                </div>
+                <div class="fotos_cambioDatos-container">
+                    <label for="nueva_foto">Nueva Foto de Perfil:</label>
+                    <input type="file" id="nueva_foto" name="nueva_foto" accept="image/*" multiple="false">
+                </div>
+                <div>
+                    <button type="submit" name="cambiar_datos">Cambiar Datos</button>
+                </div>
+            </div>
+        </div>
+
+        EOF;
+        return $html;
+    }
+
+    protected function procesaFormulario(&$datos)
+    {
+        // Obtiene los nuevos datos introducidos por el usuario
+        // $nuevoNombre = trim($datos['nuevo_nombre'] ?? '');
+        // Lista blanca
+        if (isset($datos['nuevo_nombre']) && !empty(trim($datos['nuevo_nombre']))) {
+            // Obtiene los nuevos datos introducidos por el usuario
+            $nuevoNombre = trim($datos['nuevo_nombre']);
+
+            // Lista blanca
+            if (!preg_match('/^[a-zA-Z0-9_-]+$/', $nuevoNombre)) {
+                $this->errores['nuevo_nombre'] = 'El nombre solo puede contener letras, números y guiones';
+            }
+
+            if (!isset($this->errores['nuevo_nombre'])) {
+                // Comprobamos que no exista un usuario con ese nombre
+                $user = Usuario::buscaUsuario($nuevoNombre);
+                if ($user) {
+                    error_log('nombre_duplicado');
+                    $this->errores['nuevo_nombre'] = 'Alguien ya utiliza ese nombre de usuario';
+                } else {
+                    // $_SESSION['nombre'] = $nuevoNombre;
+                    $result = Usuario::cambiarNombre($this->id, $nuevoNombre);
+                }
+            }
+        }
+
+
+        $nuevoEmail = trim($datos['nuevo_email'] ?? '');
+        if (!empty($nuevoEmail)) {
+            $nuevoEmail = filter_var($nuevoEmail, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            if (!filter_var($nuevoEmail, FILTER_VALIDATE_EMAIL)) {
+                $this->errores['nuevo_email'] = 'El email no es válido';
+            } else {
+                // $_SESSION['email'] = $nuevoEmail;
+                $result = Usuario::cambiarEmail($this->id, $nuevoEmail); 
+            }
+        }
+
+
+        //Procesamo la foto que ha escogido cargar el usuario
+        if (isset($_FILES['nueva_foto'])) {
+            $filetype = pathinfo($_FILES['nueva_foto']['name'], PATHINFO_EXTENSION);
+            $filename = uniqid() . "." . $filetype;
+            $targetFilePath = 'img/fotosPerfil/' . $filename;
+            $filesize = $_FILES['nueva_foto']['name'];
+
+            if ($filesize > 1000000) {
+                $this->errores['nueva_foto'] = 'La imagen no puede ocupar más de 1 MB';
+            }
+
+            $allowTypes = array('jpg', 'png', 'jpeg');
+            if (in_array($filetype, $allowTypes)) { //Comprobamos que la extensión de la imagen se ajusta a las requeridas
+                if (move_uploaded_file($_FILES['nueva_foto']["tmp_name"], $targetFilePath)) {
+                    //Actualizar la foto en la base de datos
+                    $result = Usuario::actualizaFoto($this->id, $targetFilePath);
+
+                    // Actualizar la foto en la sesión
+                    // $_SESSION['fotoPerfil'] = $targetFilePath;
+                } else {
+                    $this->errores['nueva_foto'] = 'Hubo un error al subir el fichero';
+                }
+            } else {
+                $this->errores['nueva_foto'] = 'La imagen que ha seleccionado debe tener extensión .jpg, .png o .jpeg';
+            }
+        }
+         header("Location: admin_usuarios.php");
+         exit(); 
+    }
+
+}
