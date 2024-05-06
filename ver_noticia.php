28e18e8277a4d0196ba1782925d6c1514d2b0909
Jorizar <jorgmari@ucm.es>
2024-05-06 12:13:34 +0200

    Modificada la vista premium de las noticias

--- ver_noticia.php
+++ ver_noticia.php
@@ -1,26 +1,30 @@
 <?php
 require_once __DIR__.'/includes/config.php';
+use es\ucm\fdi\aw\Aplicacion;
+use es\ucm\fdi\aw\noticias\Noticia;
 
 // Verificar si se proporcionó un ID de noticia en la URL
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $idNoticia = $_GET['id'];
 
     // Obtener la conexión a la base de datos desde la instancia de la aplicación
-    $app = \es\ucm\fdi\aw\Aplicacion::getInstance();
+    $app = Aplicacion::getInstance();
     $conexion = $app->getConexionBd();
 
     // Consultar la base de datos para obtener la noticia con el ID proporcionado
-    $query = "SELECT titulo, texto FROM noticias WHERE post_id = ?";
-    $stmt = $conexion->prepare($query);
-    $stmt->bind_param('i', $idNoticia);
-    $stmt->execute();
-    $stmt->store_result();
+    $smnt = Noticia::buscaPorId($idNoticia);
 
-    if ($stmt->num_rows > 0) {
+    if ($smnt) {
         // Si se encontró la noticia, obtener sus detalles
-        $stmt->bind_result($tituloNoticia, $contenidoNoticia);
-        $stmt->fetch();
 
+        $rol = $smnt->getRol();
+
+        if ($rol == 1 && (!$app->usuarioLogueado() || $_SESSION['rol'] == "free")){
+            header('Location: index.php');
+            exit();
+        }
+        $tituloNoticia = $smnt->getTitulo();
+        $contenidoNoticia = $smnt->getTexto();
         // Construir el contenido de la página
         $tituloPagina = $tituloNoticia;
         $contenidoPrincipal = "<h2>$tituloNoticia</h2>";
@@ -31,7 +35,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
         $contenidoPrincipal = '<p>La noticia solicitada no se encontró.</p>';
     }
 
-    $stmt->close();
+
 } else {
     // Si no se proporcionó un ID de noticia válido, redirigir a la página de inicio
     header('Location: index.php');
