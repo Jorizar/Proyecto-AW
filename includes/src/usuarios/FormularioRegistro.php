33bdf18123ef208be9b6630376bbc49d09884973
fedelm8 <fedelmerino8@gmail.com>
2024-05-10 19:27:52 +0200

    registro

--- includes/src/usuarios/FormularioRegistro.php
+++ includes/src/usuarios/FormularioRegistro.php
@@ -66,8 +66,6 @@ class FormularioRegistro extends Formulario
             <div>
                 <button type="submit" name="registro">Registrar</button>
             </div>
-
-        </fieldset>
         <script type="text/javascript" src="js/jquery-3.7.1.min.js"></script>
         <script type="text/javascript" src="js/validarFormulario.js"></script>
         EOF;
